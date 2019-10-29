2c5a9c4d8da4a63c7fbf4c51eb89cdaf649e1e32
timurmakarov <hi@timurmakarov.ru>
2019-10-29 14:29:01 +0300

    Add: card background image. Add admin dashboard stylize file

--- timurmakarov/apps/projects/admin.py
+++ timurmakarov/apps/projects/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Snippet
 
 from .models import Project
 
 admin.site.register(Project)
+
+admin.site.site_header = 'Timur Makarov Dashboard'
+admin.site.register(Snippet)
--- timurmakarov/apps/projects/models.py
+++ timurmakarov/apps/projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
 	project_type = models.CharField('Тип проекта', max_length = 36)
 	project_description = models.TextField('Описание проекта')
 	project_body = models.TextField('Тело проекта')
-	project_cover = models.ImageField('Обложка проекта', upload_to='upload', height_field=None, width_field=None, max_length=120)
+	project_cover = models.ImageField('Обложка проекта', upload_to='upload')
 	pub_date = models.DateTimeField('Дата публикации')
 
 	def __str__(self):
@@ -15,3 +15,15 @@ class Project(models.Model):
 	class Meta:
 		verbose_name = 'Проект'
 		verbose_name_plural = 'Проекты'
+
+class Snippet(models.Model):
+	title = models.CharField(max_length = 120)
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add = True)
+
+	def __str__(self):
+		return self.title
+
+	def body_preview(self):
+		return self.body[:50]
+		
